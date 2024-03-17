dfbcb0b72efc6d231c76540ea20d4a5b2675b2c6
RychuKropkaExe <jozek.melanczuk@gmail.com>
2024-03-17 23:13:26 +0100

    Lista 1 zad 2

--- Lista1/zad2.py
+++ Lista1/zad2.py
@@ -3,7 +3,76 @@ import numpy as np
 import sys
 from sklearn.linear_model import LinearRegression
 from matplotlib import pyplot as plt
+import random
 np.set_printoptions(threshold=sys.maxsize)
+
+rating_data_iter = []
+rating_data = []
+with open('ratings.csv','r') as f:
+    rating_iter = csv.reader(f,
+                           delimiter = ',',
+                           quotechar = '"')
+    rating_data = [rating for rating in rating_iter]
+
+rating_db = np.asarray(rating_data)
+
+m = 10000
+
+max_movie_id = 0
+max_user_id = 610
+#max_user_id = 10
+
+movies_counters = np.zeros(m)
+
+for i in range(1,len(rating_db)):
+    if int(rating_db[i][1]) <= m:
+        movies_counters[int(rating_db[i][1])-1] += 1
+    if int(rating_db[i][1]) > max_movie_id and int(rating_db[i][1]) <= m:
+        max_movie_id = int(rating_db[i][1])
+
+counter = 0
+for i in range(m):
+    if movies_counters[i] > 0:
+        counter+=1
+
+my_ratings = np.zeros((max_movie_id,1))
+
+users_ratings_matrix = np.zeros((max_user_id, max_movie_id))
+for i in range(1,len(rating_db)):
+    if int(rating_db[i][1]) <= m and int(rating_db[i][0]) <= max_user_id:
+        users_ratings_matrix[int(rating_db[i][0])-1][int(rating_db[i][1])-1] = rating_db[i][2]
+
+
+for i in range(len(my_ratings)):
+    if random.random() <= 0.33:
+        my_ratings[i] = random.randint(3, 5)
+
+my_ratings_norms = np.nan_to_num(np.linalg.norm(my_ratings))
+my_ratings_normalized = np.nan_to_num(my_ratings/my_ratings_norms)
+
+users_ratings_matrix_norms = np.nan_to_num(np.linalg.norm(users_ratings_matrix, axis=0))
+
+users_ratings_matrix_normalized = np.nan_to_num(users_ratings_matrix/users_ratings_matrix_norms)
+
+cosine_we_users=np.dot(users_ratings_matrix_normalized, my_ratings_normalized)
+
+our_profile = np.nan_to_num(cosine_we_users/np.linalg.norm(cosine_we_users))
+
+result = np.dot(users_ratings_matrix_normalized.T, our_profile)
+
+result_with_movies_id = np.zeros((max_movie_id, 2))
+
+for i in range(max_movie_id):
+    result_with_movies_id[i][0] = result[i]
+    result_with_movies_id[i][1] = i
+
+result_with_movies_id = result_with_movies_id.tolist()
+
+def myKeyComp(e):
+    return e[0]
+
+sorted_results = sorted(result_with_movies_id, key=myKeyComp, reverse=True)
+
 movies_data_iter = []
 movies_data = []
 with open('movies.csv','r') as f:
@@ -15,69 +84,11 @@ with open('movies.csv','r') as f:
 for i in range(1,len(movies_data)):
     movies_data[i][0] = int(movies_data[i][0])
 
-movies_db = np.asarray(movies_data)
+movies_names_db = [None]*max_movie_id
+for i in range(1,len(movies_data)):
+    if int(movies_data[i][0]) <= max_movie_id: 
+        movies_names_db[int(movies_data[i][0])-1] = movies_data[i][1]
 
-rating_data_iter = []
-rating_data = []
-with open('ratings.csv','r') as f:
-    rating_iter = csv.reader(f,
-                           delimiter = ',',
-                           quotechar = '"')
-    rating_data = [rating for rating in rating_iter]
-
-for i in range(1,len(rating_data)):
-    rating_data[i][0] = int(rating_data[i][0])
-
-rating_db = np.asarray(rating_data)
-users_array = []
-
-for i in range(1,len(rating_db)):
-    if rating_db[i][1] == '1':
-        users_array.append(rating_db[i][0])
-#print(users_array)
-
-m = 10000
-
-users_ratings_matrix = np.zeros((len(users_array), m))
-toy_story_ratings_matrix = np.zeros(len(users_array))
-
-for i in range(0, len(users_array)):
-    for j in range(1,len(rating_db)):
-        if rating_db[j][0] == users_array[i] and int(rating_db[j][1]) <= m and int(rating_db[j][1]) > 1 :
-            users_ratings_matrix[i][int(rating_db[j][1])-2] = rating_db[j][2]
-        if rating_db[j][0] == users_array[i] and int(rating_db[j][1]) == 1 :
-            toy_story_ratings_matrix[i] = rating_db[j][2]
-
-# print(users_ratings_matrix)
-# print(toy_story_ratings_matrix)
-            
-training_ratings_matrix = users_ratings_matrix[:200,:]
-training_toy_story_ratings = toy_story_ratings_matrix[:200]
-
-test_X = users_ratings_matrix[200:215,:]
-test_Y = toy_story_ratings_matrix[200:215]
-
-plot_limit_m = 10000
-
-scores_normal = []
-for i in range(10,plot_limit_m,100):
-    training_ratings_matrix = users_ratings_matrix[:200,:i]
-    training_toy_story_ratings = toy_story_ratings_matrix[:200]
-    test_ratings_matrix = users_ratings_matrix[:,:i]
-    test_toy_story_ratings = toy_story_ratings_matrix[:]
-    clf = LinearRegression().fit(training_ratings_matrix, training_toy_story_ratings)
-    scores_normal.append(clf.score(test_ratings_matrix, test_toy_story_ratings))
-
-steps = [step for step in range(10,plot_limit_m,100)]
-
-#print(steps)
-
-print(scores_normal)
-# plot
-fig, ax = plt.subplots()
-
-ax.plot(steps, scores_normal, linewidth=1.0)
-
-plt.savefig('plot_200_step_100.png', dpi=1200)
-print(scores_normal)
+for i in range(10):
+    print("RECOMENDED MOVIE: ", movies_names_db[int(sorted_results[i][1])], " WITH VALUE: ", sorted_results[i][0])
 
